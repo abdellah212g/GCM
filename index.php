1a45a291e6a6425c6ceac8a00a868290f6cc0c86
Iliass Raihani <i.raihani@outlook.com>
2020-10-31 21:28:04 +0100

    htacces

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php 
 include_once "core/init.php";
 
-include_once('views/home_view.php');
+//definition de la page courante
+if(isset($_GET['page']) AND !empty($_GET['page'])){
+    $page = trim(strtolower($_GET['page'])); //HOME
+ }
+ else {
+    $page = 'home';
+ }
+
+ //array contenant toutes les pages
+$all_pages = scandir('controllers/');
+
+if(in_array($page.'_controller.php', $all_pages)){
+    include_once 'models/'.$page.'_model.php';
+    include_once 'controllers/' . $page . '_controller.php';
+    include_once 'views/' . $page . '_view.php';
+}else {
+    header("Location: 404.php");
+}
