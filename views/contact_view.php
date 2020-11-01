153918c7b84e7dc0ee9005edc8ceb0f8c94e7063
Iliass Raihani <i.raihani@outlook.com>
2020-11-01 21:52:05 +0100

    styles

--- views/contact_view.php
+++ views/contact_view.php
@@ -4,7 +4,7 @@
 <div class="contact-page">
   <div class="contact-form">
     <form  enctype="multipart/form-data" method="post">
-      <input name="fname" type="text" placeholder="First Name" required/>
+      <input name="fname" type="text" placeholder="First Name" required autofocus/>
       <input name="lname" type="text" placeholder="Last Name" required/>
       <input name="phone" type="tel" placeholder="Phone Number" pattern="[0-9]{10}" required/> 
       <textarea name="msg" type="text" placeholder="Your message" required></textarea>
--- views/includes/sidebar.php
+++ views/includes/sidebar.php
@@ -5,14 +5,18 @@
   </div>
   <div class="w3-bar-block">
     <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>&nbsp; Close Menu</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-users fa-fw"></i>&nbsp; Overview</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>&nbsp; Views</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>&nbsp; Traffic</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bullseye fa-fw"></i>&nbsp; Geo</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-diamond fa-fw"></i>&nbsp; Orders</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bell fa-fw"></i>&nbsp; News</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>&nbsp; General</a>
-    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-history fa-fw"></i>&nbsp; History</a>
+    <?php if($access == 3 || $access == 4): ?>
+    <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-eye fa-fw"></i>&nbsp; Overview</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>&nbsp; Patients</a>
+    <?php endif;?>
+    <?php if($access == 3 || $access == 2 || $access == 4): ?>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-calendar fa-fw"></i>&nbsp; Calendar</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw"></i>&nbsp; Messages</a>
+    <?php endif;?>
+    <?php if($access == 1 || $access == 4): ?>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-heartbeat fa-fw"></i>&nbsp; Medical Record</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-thumb-tack fa-fw"></i>&nbsp; Appointment</a>
+    <?php endif;?>
     <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i>&nbsp; Settings</a><br><br>
   </div>
 </nav>
