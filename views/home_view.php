83385bcdd89f23533bdbca440643de12d5a7891d
Iliass Raihani <i.raihani@outlook.com>
2020-11-01 07:01:24 +0100

    init sections

--- views/home_view.php
+++ views/home_view.php
@@ -43,8 +43,4 @@
   </div>
 </div>
 
-<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">Bienvenue chez Médical DU BLED</h1>
-</div>
-
 <?php include_once 'includes/footer.php' ?>
