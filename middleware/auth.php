c4e9b04db42ca1065d2a400b4bfb515ea9024e9b
Iliass Raihani <i.raihani@outlook.com>
2020-11-02 15:06:18 +0100

    update login module

--- middleware/auth.php
+++ middleware/auth.php
@@ -2,8 +2,7 @@
 if(!empty($_SESSION['username']))
 {
     $page = $_GET['page'];
+    $access = $db->fetchValue('access', 'users', 'username', $_SESSION['username']);
 } else {
     header("Location: 404.php");
 }
-
-$access = $db->fetchValue('access', 'users', 'username', $_SESSION['username']);
