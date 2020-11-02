2d6f0c41cc71b6cbf98d6fef3b7400efe9f099cf
Iliass Raihani <i.raihani@outlook.com>
2020-11-02 14:25:56 +0100

    correction

--- views/login_view.php
+++ views/login_view.php
@@ -1,8 +1,6 @@
 <?php include_once 'includes/header.php' ?>
 <?php include_once 'includes/navbar.php' ?>
 
-<meta > 
-
 <div class="login-page">
   <div class="form">
     <form class="register-form" method="post">
