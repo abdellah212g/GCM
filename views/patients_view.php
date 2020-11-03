52dda23f74c6ba56c5cc947e53140ae1faf39fbb
Iliass Raihani <i.raihani@outlook.com>
2020-11-03 21:40:16 +0100

    Update patients_view.php

--- views/patients_view.php
+++ views/patients_view.php
@@ -6,13 +6,13 @@ $patients_name = $db->selectColumn('username', 'users', 'access', '1');
 ?>
 
 <div class="w3-container">
-    <h5>Patients</h5>
-    <ul class="w3-ul w3-card-4 w3-white">
+  <h5>Patients</h5>
+  <ul class="w3-ul w3-card-4 w3-white">
     <?php foreach($patients_name as $name): ?>
-      <li class="w3-padding-16">
-        <img src="assets/img/profile.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
-        <span class="w3-xlarge"><?= $name ?></span><br>
-      </li>
+    <li class="w3-padding-16">
+      <img src="assets/img/profile.png" class="w3-left w3-circle w3-margin-right" style="width:35px">
+      <span class="w3-xlarge"><?= $name ?></span><br>
+    </li>
     <?php endforeach; ?>
-    </ul>
-  </div>
+  </ul>
+</div>
