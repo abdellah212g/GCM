2eed0ba5c6a66982f33a554d973680b2dc1bbe13
Iliass Raihani <i.raihani@outlook.com>
2020-11-09 21:32:56 +0100

    erebos methods example

--- core/init.php
+++ core/init.php
@@ -4,3 +4,17 @@ include_once 'helpers/functions.php';
 include_once 'core/classes/Erebos.php';
 
 $db = new Erebos('localhost', 'gcm', 'root', '');
+
+// $result = $db->selectValue('timestamp', 'users', 'username', 'hamza');
+// $result = $db->updateValue('users', 'access', 1, 'username', 'salim');
+
+// $result = $db->selectRow('users', 'username', 'hamza');
+// $newUser = array('username'=>'kador', 'email'=>'kador@example.com', 'password'=>'aze');
+// $result = $db->insertRow('users', ':username, :email, :password', $newUser);
+// $result = $db->deleteRow('users', 'username', 'lain');
+
+// $result = $db->selectColumn('username', 'users');
+
+// debug($result);
+
+// exit;
